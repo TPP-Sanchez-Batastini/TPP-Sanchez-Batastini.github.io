9fd82f0fa7827295827ae2b6a78250c5d5355902
Franco Batastini <fbatastini@fi.uba.ar>
2023-02-11 19:38:54 -0300

    [ADD] Main Menu & Routes

--- src/Menus/MainMenu.jsx
+++ src/Menus/MainMenu.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+
+const styleMenu = {
+  width:"100vw", 
+  height:"100vh", 
+  backgroundImage:`url("DrivingSimMenuNoTitle.png")`, 
+  backgroundSize:"cover", 
+  backgroundRepeat: "no-repeat"
+}
+
 
 export const MainMenu = () => {
-  return (
-    
-        <>
-        <div>MainMenu</div>
-        </>
 
-  )
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <div style={styleMenu}>
+        <div id="button-box" style={{position:"absolute", top:"5%", right:"5%"}}>
+          <div className={"MainMenuTitle"}>
+            <h1 style={{textAlign:"center", fontSize:"7rem"}}>
+              Driving<br/>Simulator
+            </h1>
+          </div>
+          <div className={"buttonWrapperMainMenu"}>
+            <button 
+              className={"buttonMainMenu"}
+              onClick={() => navigate("/scene")}
+            >
+              Iniciar
+            </button>
+          </div>
+          <div className={"buttonWrapperMainMenu"}>
+          <button 
+              className={"buttonMainMenu"}
+              onClick={() => navigate("/level-editor")}
+            >
+              Editor de Niveles
+            </button>
+          </div>
+          <div className={"buttonWrapperMainMenu"}>
+            <button 
+              className={"buttonMainMenu"}
+              onClick={() => navigate("/configs")}
+            >
+              Configuración
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
