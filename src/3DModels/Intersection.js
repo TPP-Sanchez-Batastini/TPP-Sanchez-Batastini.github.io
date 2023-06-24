88eab303125e12a03311906ed96e2e49105bfe59
Franco Batastini <fbatastini@fi.uba.ar>
2023-06-24 20:56:50 -0300

    [FIX] Fix vertices mesh

--- src/3DModels/Intersection.js
+++ src/3DModels/Intersection.js
@@ -43,20 +43,20 @@ export default class Intersection extends VisualEntity{
     }
 
     createSidewalkCurve(){
-        let initialPoint = new THREE.Vector3(0,0,0);
-        const lerpPoints = this.getLerpPoints(initialPoint, new THREE.Vector3(0,0,6), 5);
+        let initialPoint = new THREE.Vector3(-5,0,-5);
+        const lerpPoints = this.getLerpPoints(initialPoint, new THREE.Vector3(-5,0,1), 5);
         const curve = new THREE.QuadraticBezierCurve(
-            new THREE.Vector2(0,6),
-            new THREE.Vector2(0,10),
-            new THREE.Vector2(4,10)
+            new THREE.Vector2(-5,1),
+            new THREE.Vector2(-5,5),
+            new THREE.Vector2(-1,5)
         );
         const pointsCurve = curve.getPoints(10);
         for (let i=0; i<pointsCurve.length; i++){
             pointsCurve[i] = new THREE.Vector3(pointsCurve[i].x, 0, pointsCurve[i].y);
         }
-        const secondLerpPoints = this.getLerpPoints(new THREE.Vector3(4,0,10), new THREE.Vector3(10,0,10), 5);
-        const thirdLerpPoints = this.getLerpPoints(new THREE.Vector3(10,0,10), new THREE.Vector3(10,0,0), 5);
-        const fourthLerpPoints = this.getLerpPoints(new THREE.Vector3(10,0,0), new THREE.Vector3(0,0,0), 5);
+        const secondLerpPoints = this.getLerpPoints(new THREE.Vector3(-1,0,5), new THREE.Vector3(5,0,5), 5);
+        const thirdLerpPoints = this.getLerpPoints(new THREE.Vector3(5,0,5), new THREE.Vector3(5,0,-5), 5);
+        const fourthLerpPoints = this.getLerpPoints(new THREE.Vector3(5,0,-5), new THREE.Vector3(-5,0,-5), 5);
         return [
             initialPoint, 
             ...lerpPoints, 
@@ -71,24 +71,34 @@ export default class Intersection extends VisualEntity{
     getGeometrySidewalk(){
         const geom = new THREE.BufferGeometry();
         const points = this.createSidewalkCurve();
-        const vertices = [];
+        const columnas = points.length;
+        const filas = 3; //Fila baja, fila alta, tapa alta
+        const aux_vertices = [[],[],[]];
         const indexes = [];
         for (let i=0; i<points.length; i++){
-            vertices.push(
-                points[i].x, points[i].y, points[i].z,
-                points[i].x, points[i].y+this.SIDEWALK_HEIGHT, points[i].z
-            );
+            aux_vertices[0].push(points[i].x, points[i].y, points[i].z);
+            aux_vertices[1].push(points[i].x, this.SIDEWALK_HEIGHT, points[i].z);
+            aux_vertices[2].push(5, this.SIDEWALK_HEIGHT, 5);
         }
-        //REVISAR ESTOS VERTICES
-        vertices.push(new THREE.Vector3(5,this.SIDEWALK_HEIGHT,5));
-        for (let i=0; i<points.length*4-3; i++){
-            indexes.push(i, i+1, i+2);
+        const vertex = [...aux_vertices[0], ...aux_vertices[1], ...aux_vertices[2]];
+        for (let i=0; i < filas-1; i++) {
+            for (let j=1; j < columnas; j++) {
+                //1er triangulo
+                indexes.push(i*columnas+j-1);
+                indexes.push((i+1)*columnas+j-1);
+                indexes.push(i*columnas+j);
+                //2do triangulo
+                indexes.push((i+1)*columnas+j-1);
+                indexes.push(i*columnas+j);
+                indexes.push((i+1)*columnas+j);
+                if ((i*columnas + j) % columnas === 0 && (j !== 0) && (i*columnas+j) !== (filas*columnas-1)){
+                    indexes.push((i+1)*columnas+j);
+                    indexes.push((i+1)*columnas);
+                }
+            }
         }
-        //REVISAR ESTOS INDICES
-        for (let i=0; i<points.length-3; i++){
-            indexes.push(i*2+1,vertices.length%3-1,i*2+3);
-        }
-        const vertexArr = new Float32Array(vertices);
+        console.log(indexes);
+        const vertexArr = new Float32Array(vertex);
         geom.setIndex(indexes);
         geom.setAttribute("position", new THREE.BufferAttribute(vertexArr, 3));
         return geom;
@@ -101,7 +111,7 @@ export default class Intersection extends VisualEntity{
         //    this.pathToNormalMap
         //);
         //const materialSidewalk = new THREE.MeshStandardMaterial( {map: texture,  side: THREE.DoubleSide} );
-        const materialSidewalk = new THREE.MeshBasicMaterial({color:"#ff0000", side: THREE.DoubleSide});
+        const materialSidewalk = new THREE.MeshBasicMaterial({color:0xefefef, side: THREE.DoubleSide});
         //materialSidewalk.normalMap = normalTexture;
         const geometrySidewalk = this.getGeometrySidewalk();
 
@@ -123,20 +133,28 @@ export default class Intersection extends VisualEntity{
     async addToScene(scene, objectName, position){
         if(!this.threeDModel){
             const baseStreet = this.createStreetMesh();
-            const sidewalks = [];
-            for (let i=0; i<4; i++){
-                const sidewalk = this.createSidewalkMesh();
-                sidewalk.position.set(30*i%2,1,30*i%2);
-                sidewalk.updateMatrix();
-                sidewalk.updateMatrixWorld();
-                sidewalk.matrixAutoUpdate = false;
-                sidewalks.push(sidewalk);
-            }
-            
             baseStreet.position.set(0,0,0);
             this.threeDModel = new THREE.Group();
             this.threeDModel.add(baseStreet);
-            sidewalks.forEach(sidewalk => this.threeDModel.add(sidewalk));
+            for (let i=0; i<4; i++){
+                const sidewalk = this.createSidewalkMesh();
+                if (i==0){
+                    sidewalk.position.set(-this.SIZE/2+5,1,-this.SIZE/2+5);
+                    sidewalk.rotateOnAxis(new THREE.Vector3(0,1,0), Math.PI/2);
+                }else if(i==2){
+                    sidewalk.position.set(-this.SIZE/2+5,1,this.SIZE/2-5);
+                    sidewalk.rotateOnAxis(new THREE.Vector3(0,1,0), Math.PI);
+                }else if(i==3){
+                    sidewalk.position.set(this.SIZE/2-5,1,-this.SIZE/2+5);
+                }else{
+                    sidewalk.position.set(this.SIZE/2-5,1,this.SIZE/2-5);
+                    sidewalk.rotateOnAxis(new THREE.Vector3(0,1,0), -Math.PI/2);
+                }
+                sidewalk.updateMatrix();
+                sidewalk.updateMatrixWorld();
+                sidewalk.matrixAutoUpdate = false;
+                this.threeDModel.add(sidewalk);
+            }
             this.threeDModel.name = objectName;
             this.threeDModel.position.set(position[0], position[1], position[2]);
             baseStreet.updateMatrix();
